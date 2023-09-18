74e719f131c3eb859b2f7f9acec036ffcd319ca9
Carlo Ferrero <ferrerocarlo96@gmail.com>
2023-09-18 22:27:43 +0200

    ADD(example): example folder

--- src/route.jsx
+++ src/route.jsx
@@ -3,6 +3,9 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "@pages/App";
 import ErrorPage from "@pages/404";
 import Projects from "./pages/Projects";
+import HooksExaple from "./example/useState";
+import EventHandling from "./example/eventsHandling";
+import SchoolAttendance from "./example/conditionalRendering";
 
 export const router = createBrowserRouter([
     {
@@ -13,6 +16,21 @@ export const router = createBrowserRouter([
     {
         path: "/projects",
         element: <Projects />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/hooks-example",
+        element: <HooksExaple />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/events-example",
+        element: <EventHandling />,
+        errorElement: <ErrorPage />,
+    },
+    {
+        path: "/conditional-rendering-example",
+        element: <SchoolAttendance />,
         errorElement: <ErrorPage />,
     },
 ]);
